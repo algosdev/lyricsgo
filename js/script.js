7f3c07a7b8043fbaba02d9f6ba8b7313a04b1b68
prince-algo <samandarofficial149@gmail.com>
2020-08-03 21:38:07 +0500

    Uploading

--- js/script.js
+++ js/script.js
@@ -3,6 +3,23 @@ const result = document.getElementById('result');
 const inp = document.getElementById('inp');
 const more = document.getElementById('pagination');
 const loader = document.querySelector('#loading');
+let first = (localStorage.getItem("first") === null) ? true : localStorage.getItem("first");
+if (first == true) {
+    setTimeout(() => {
+        if (first) {
+            window.onload = whenOnload();
+            localStorage.setItem('first', 'false');
+        }
+    }, 5300);
+}
+else {
+    whenOnload();
+}
+function whenOnload() {
+    console.log("Loaded");
+    document.querySelector(".preload-cont").style.display = "none";
+    document.querySelector("body").style.overflow = "visible";
+}
 const apiURL = 'https://api.lyrics.ovh';
 async function getMoreSongs(url) {
     loader.classList = '';
@@ -13,30 +30,32 @@ async function getMoreSongs(url) {
     loader.classList = 'hide';
     console.log('Loader Visible');
     organizeData(data);
-  }
-async function getLyrics(a, b){
+}
+async function getLyrics(a, b) {
     loader.classList = '';
     result.innerHTML = '';
     more.innerHTML = "";
     const res = await fetch(`${apiURL}/v1/${a}/${b}`);
     const data = await res.json();
     console.log(data.lyrics);
-    if(!data.hasOwnProperty('lyrics')){
+    if (!data.hasOwnProperty('lyrics')) {
         loader.classList = 'hide';
         result.innerHTML = `<p>Sorry, the lyric has been removed or modified from server</p>
         <p>It's not Algo's mistake</p>
         <p>Take another stab</p>`;
-        more.innerHTML = "";}
+        more.innerHTML = "";
+    }
     else {
         const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
         loader.classList = 'hide';
         console.log('Loader Hidden');
         result.innerHTML = `<h2><strong>${a}</strong>  -  ${b}</h2>
     <span>${lyrics}</span>`;
-    more.innerHTML = "";}
+        more.innerHTML = "";
+    }
 
 }
-async function organizeData(song){
+async function organizeData(song) {
     loader.classList = 'hide';
     result.innerHTML = '';
     more.innerHTML = "";
@@ -48,22 +67,22 @@ async function organizeData(song){
           data-songTitle='${e.title}'>View</button></li>`;
     });
     result.innerHTML = `<i>${song.total} results have been found</i><ul>${output}</ul>`;
-    if(song.prev && song.next){
-     more.innerHTML = `<button class='page' onclick='getMoreSongs("${song.prev}")'>Prev</button><button class='page'
+    if (song.prev && song.next) {
+        more.innerHTML = `<button class='page' onclick='getMoreSongs("${song.prev}")'>Prev</button><button class='page'
       onclick='getMoreSongs("${song.next}")'>Next</button>`;
     }
-    else if(song.prev){
+    else if (song.prev) {
         more.innerHTML = `<button class='page' onclick='getMoreSongs("${song.prev}")'>Prev</button>`;
     }
-    else if(song.next){
+    else if (song.next) {
         more.innerHTML = `<button class='page' onclick='getMoreSongs("${song.next}")'>Next</button>`;
     }
     else {
         more.innerHTML = "";
     }
 }
-async function findSong(inf){
-    loader.classList ='';
+async function findSong(inf) {
+    loader.classList = '';
     result.innerHTML = '';
     more.innerHTML = "";
     console.log('Loader Visible');
@@ -74,16 +93,16 @@ async function findSong(inf){
 form.addEventListener('submit', e => {
     e.preventDefault();
     const inpValue = inp.value.trim();
-    if(!inpValue){
+    if (!inpValue) {
         alert('Please enter artist or song name!');
     }
     else {
-       findSong(inpValue);
+        findSong(inpValue);
     }
 });
 result.addEventListener('click', e => {
     const btn = e.target;
-    if(btn.tagName == 'button');{
+    if (btn.tagName == 'button'); {
         const artist = btn.getAttribute('data-artist');
         const songName = btn.getAttribute('data-songTitle');
         getLyrics(artist, songName);
